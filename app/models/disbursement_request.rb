169979ba2d10586c87f7ce71a7f106d242d23013
jais anasrulloh <jaisanas2@gmail.com>
2019-10-31 23:12:21 +0700

    add employee to disbursement_request association

--- app/models/disbursement_request.rb
+++ app/models/disbursement_request.rb
@@ -1,2 +1,3 @@
 class DisbursementRequest < ApplicationRecord
+    belongs_to :employee
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,3 @@
 class Employee < ApplicationRecord
+    has_many :disbursement_requests, dependent: :destroy
 end
