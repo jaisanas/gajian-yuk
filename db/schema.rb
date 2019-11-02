2293c58e03b257c7c7c33d6cf4030ee940cccd3e
jais <jais@kargo.tech>
2019-11-02 13:23:00 +0700

    modify employee deposit index and modify employee index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191031160655) do
+ActiveRecord::Schema.define(version: 20191102055704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20191031160655) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["employees_id"], name: "index_disbursement_requests_on_employees_id"
+  end
+
+  create_table "disbursement_transaction_logs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "disbursement_transactions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "employees", force: :cascade do |t|
